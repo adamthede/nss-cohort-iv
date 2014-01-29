32b63ff6bdf4ec3c4098bae5acd9f43f98f91890
Adam Thede <adam@thedetech.com>
2014-01-28 20:49:15 -0600

    added anchor tags with data-search attributes to my table of animals

--- 2014-01-28-OOP.Animal-Shelter/public/js/source/main.js
+++ 2014-01-28-OOP.Animal-Shelter/public/js/source/main.js
@@ -1,27 +1,60 @@
+/* global Animal, animalFactory: false */
+
 (function(){
 
   'use strict';
 
   $(document).ready(initialize);
 
+  var animals = [];
+
   function initialize(){
     $('input, textarea').focusin(focusInput);
     $('input, textarea').blur(blurInput);
     $('#add-photo').click(addPhoto);
     $('#add-animal').click(addAnimal);
+
+    animals = animalFactory();
+    populateTableAnimals();
+  }
+
+  function populateTableAnimals(){
+    for(var i = 0; i < animals.length; i++){
+      var $a1 = $('<a>');
+      var $a2 = $('<a>');
+      var $a3 = $('<a>');
+      var $a4 = $('<a>');
+
+      var $tdname         = $('<td>').text(animals[i].name);
+      var $tdage          = $('<td>').append($a1.attr({'href': '#', 'data-search': 'age', 'data-value': animals[i].age}).text(animals[i].age));
+      var $tdgender       = $('<td>').append($a2.attr({'href': '#', 'data-search': 'gender', 'data-value': animals[i].gender}).text(animals[i].gender));
+      var $tdphotos       = $('<td>').css('background-image', animals[i].photos[0]);
+      var $tddescription  = $('<td>').text(animals[i].description);
+      var $tdcolor        = $('<td>').append($a3.attr({'href': '#', 'data-search': 'color', 'data-value': animals[i].color}).text(animals[i].color));
+      var $tdspecies      = $('<td>').append($a4.attr({'href': '#', 'data-search': 'species', 'data-value': animals[i].species}).text(animals[i].species));
+      var $tr             = $('<tr>');
+
+      $tdphotos.addClass('pic');
+      $tdphotos.addClass('th');
+
+      $tr.append($tdname, $tdage, $tdgender, $tdphotos, $tddescription, $tdcolor, $tdspecies);
+
+      $('tbody').append($tr);
+    }
   }
 
   function addAnimal(){
-    var species = $('#species').val();
-    var color = $('#color').val();
-    var age = $('#age').val() * 1;
-    var gender = $('#gender').val();
-    var name = $('#name').val();
+    var species     = $('#species').val();
+    var color       = $('#color').val();
+    var age         = $('#age').val() * 1;
+    var gender      = $('#gender').val();
+    var name        = $('#name').val();
     var description = $('#description').val();
-    var photos = getAnimalPhotos();
+    var photos      = getAnimalPhotos();
 
-    console.log(species, color, age, gender, name, description, photos);
-    debugger;
+    var animal = new Animal(name, age, gender, photos, description, color, species);
+    animals.push(animal);
+
     event.preventDefault();
   }
 
@@ -34,26 +67,19 @@
 
   function addPhoto(){
     var img = $('#photo').val();
-    //var $img = $('<img>');
-    //$img.addClass('th');
-    //$img.addClass('pic');
-    //$img.attr('src', img);
-    //$('#photodisplay').prepend($img);
-    
-    //var $a = $('<a>');
-    //$a.addClass('th radius');
-
     var $div = $('<div>');
+    
     $div.addClass('pic');
     $div.addClass('th');
     $div.css('background-image', 'url('+img+')');
-    $div.css('background-size', 'contain');
+    
     $('#photodisplay').prepend($div);
+   
     event.preventDefault();
   }
 
   function focusInput(){
-    $(this).css('background-color', 'lavender');
+    $(this).css('background-color', 'lemonChiffon');
   }
 
   function blurInput(){
