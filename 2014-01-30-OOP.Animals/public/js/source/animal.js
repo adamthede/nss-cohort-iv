289ef0a1c90cfa30076f5a15cfd28688eaf6ee0e
Adam Thede <adam@thedetech.com>
2014-01-30 15:28:51 -0600

    adding object oriented practice techniques

---
+++ 2014-01-30-OOP.Animals/public/js/source/animal.js
@@ -0,0 +1,18 @@
+/* jshint unused:false */
+
+var Animal = (function(){
+
+  'use strict';
+
+  function Animal(name, species, gender, age){
+    this.name = name;
+    this.species = species || 'not set';
+    this.gender = gender || 'not set';
+    this.age = age || 0;
+  }
+
+  return Animal;
+//adam is my partner!!!
+//nat is my partner!!don
+
+})();
